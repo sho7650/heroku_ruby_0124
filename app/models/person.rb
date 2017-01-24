01a793a4ebdc1a693fe02eb288b887e0a1ce0fe5
Takashi Abe <tabe@salesforce.com>
2017-01-24 13:26:31 +0900

    Adopted SLDS to all contents

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,5 @@
 class Person < ActiveRecord::Base
   def age
-    d1=self.birth.strftime("%Y%m%d").to_i
-    d2=Date.today.strftime("%Y%m%d").to_i
-    return (d2 - d1) / 10000
+    (Date.today.strftime("%Y%m%d").to_i - self.birth.strftime("%Y%m%d").to_i) / 10000
   end
 end
