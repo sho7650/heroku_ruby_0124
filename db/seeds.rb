0b1ccb27a150bda7e27db785e1a5d4d05dacc24e
Takashi Abe <tabe@salesforce.com>
2017-02-17 12:11:16 +0900

    Prepared default data of Amuse

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Person.create(name: "樫野 有香", birth: '1988-12-23', home: "広島県", height: 161, office: "アミューズ")
+Person.create(name: "大本 彩乃", birth: '1988-09-20', home: "広島県", height: 164, office: "アミューズ")
+Person.create(name: "西脇 綾香", birth: '1989-02-15', home: "広島県", height: 160, office: "アミューズ")
+Person.create(name: "中元 すず香", birth: '1997-12-20', home: "広島県", height: 162, office: "アミューズ")
+Person.create(name: "菊地 最愛", birth: '1999-07-04', home: "愛知県", height: 154, office: "アミューズ")
+Person.create(name: "水野 由結", birth: '1999-06-20', home: "神奈川県", height: 160, office: "アミューズ")
+
