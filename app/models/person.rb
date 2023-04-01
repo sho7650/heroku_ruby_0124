43b69e69dd828865ac9a57935da4618573d7446f
sho kisaragi <sho@oshiire.to>
2023-04-01 09:02:21 +0900

    add validation for person

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,9 @@
 class Person < ActiveRecord::Base
+  validates :name,
+    length: { minimum: 1, maximum: 256}
+  validates :birth,
+    presence: true
+
   def age
     (Date.today.strftime("%Y%m%d").to_i - self.birth.strftime("%Y%m%d").to_i) / 10000
   end
